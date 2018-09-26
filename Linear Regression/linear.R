4142941be71353adfcf4238ecb393001d2577099
Student Generic Account <ADStudent@auaad.aua.am>
2018-09-26 16:19:54 +0400

    Linear Regression class 2

--- Linear Regression/linear.R
+++ Linear Regression/linear.R
@@ -27,3 +27,23 @@ ggplot(mtcars, aes(x = hp, y = mpg))+ geom_point() +
 
 ggplot(mtcars, aes(x = hp, y = mpg)) +geom_point() +
   geom_smooth(method = 'lm')
+
+# Class 2
+
+## SStot
+ggplot(mtcars, aes(x = hp, y = mpg))+ geom_point() +
+  geom_segment(aes(xend = hp, yend = mean(mtcars$mpg)), alpha = .2, col='red') +
+  geom_hline(yintercept = mean(mtcars$mpg), col = 'blue', size = 1.5) +
+  ggtitle("Intercept only model")
+  
+## SSreg
+ggplot(mtcars, aes(x = hp, y = fitted(mod3)))+ geom_point() +
+  geom_segment(aes(xend = hp, yend = mean(mtcars$mpg)), alpha = .2, col='red') +
+  geom_hline(yintercept = mean(mtcars$mpg), col = 'blue', size = 1.5)
+
+##SSres
+ggplot(mtcars, aes(x = hp, y = mpg))+ geom_point() +
+  geom_smooth(method='lm', se = F, color = "blue") +
+  geom_segment(aes(xend = hp, yend = fitted(mod3)), alpha = .2, col='red') +
+  geom_point() + geom_point(aes(y=fitted(mod3)), shape  = 1)
+
