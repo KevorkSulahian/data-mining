b3c80f18864344e4759dc0300003fb854c0daeb3
Student Generic Account <ADStudent@auaad.aua.am>
2018-10-24 16:14:00 +0400

    oof

--- Random Forrest/random_forrest.R
+++ Random Forrest/random_forrest.R
@@ -33,3 +33,63 @@ pr.class[1:200]
 
 library(e1071)
 confusionMatrix(pr.class, test$classe)
+
+
+# different class
+
+bank <- read.csv("Random Forrest/bank-1.csv")
+sapply(bank, class)
+
+set.seed(1)
+index <- createDataPartition(bank$y, p = .8, list = F)
+train <- bank[index,]
+test <- bank[-index,]
+
+set.seed(1)
+model <- randomForest(y~., data = train, ntree = 25, do.trace = T )
+
+pr <- predict(model, test, type = "prob")
+pr[1:25,]
+
+library(ROCR)
+p_test <- prediction(pr[,2], test$y)
+perf <- performance(p_test, "tpr", "fpr")
+plot(perf)
+
+performance(p_test, "auc")@y.values
+
+set.seed(1)
+trc <- trainControl(method = "cv", number = 10)
+mtry_grid <- expand.grid(mtry = c(4,7,9,10))
+
+set.seed(1)
+train1 <- train[sample(nrow(train),5000),]
+model2 <-  train(y~., data = train1, 
+                 trControl = trc,
+                 method = "rf",
+                 ntree = 25,
+                 tuneGrid = mtry_grid)
+
+model2$results
+model2$bestTune
+
+pr <- predict(model2, newdata = test, type = "prob")
+p_test <- prediction(pr[,2], test$y)
+perf <- performance(p_test, "tpr", "fpr")
+plot(perf)
+
+performance(p_test, "auc")@y.values
+
+set.seed(1)
+trc <- trainControl(method = "cv", classProbs = T,
+                    summaryFunction = twoClassSummary, number = 5)
+
+set.seed(1)
+mm <- train(y~., data = train1,
+            trControl = trc, method = "rf",
+            ntree = 25, metric = "ROC",
+            tuneGrid = mtry_grid)
+plot(mm)
+
+varImpPlot(mm$finalModel)
+
